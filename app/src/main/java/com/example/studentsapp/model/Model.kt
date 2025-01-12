b2a4148c305ee940514f5ede641a402885a04c00
Amit Ben Yaakov <amitby98@gmail.com>
2025-01-12 14:07:41 +0200

    adding list of students with logic

---
+++ app/src/main/java/com/example/studentsapp/model/Model.kt
@@ -0,0 +1,64 @@
+package com.example.studentsapp.model
+
+import kotlin.text.set
+
+
+class Model private constructor() {
+
+    val students: MutableList<Student> = ArrayList()
+
+    companion object {
+        val shared = Model()
+    }
+
+    init {
+        for (i in 0..20) {
+            val student = Student(
+                name = "Name $i",
+                id = "Student ID: $i",
+                avatarUrl = "",
+                isChecked = false
+            )
+            students.add(student)
+        }
+    }
+    fun addStudent(student: Student) {
+        students.add(student)
+    }
+
+
+    fun getAllStudents(): List<Student> = students
+
+
+    fun getStudentById(id: String): Student? {
+        return students.find { it.id == id }
+    }
+
+
+    fun updateStudent(updatedStudent: Student) {
+        val index = students.indexOfFirst { it.id == updatedStudent.id }
+        if (index != -1) {
+            students[index] = updatedStudent
+        }
+    }
+
+
+    fun deleteStudent(id: String) {
+        students.removeAll { it.id == id }
+    }
+
+
+    fun updateStudentCheckStatus(id: String, isChecked: Boolean) {
+        val student = getStudentById(id)
+        student?.let {
+            it.isChecked = isChecked
+            updateStudent(it)
+        }
+    }
+}
+
+
+
+
+
+
